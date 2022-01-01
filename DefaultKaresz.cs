f1a77e20640ee4b7c2a33f2e7a914c08616971a8
Molnár Attila <molnar.attila@szlgbp.hu>
2022-01-01 23:55:42 +0100

    Add project files.

---
+++ DefaultKaresz.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Drawing;
+
+namespace LogoKaresz
+{
+    partial class Form1
+    {
+        const bool fel = false;
+        const bool le = true;
+        private static Avatar defaultkaresz;
+
+        private bool Kilépek_e_a_pályáról(double d) => defaultkaresz.Kilépek_e_a_pályáról(d);
+        private void Előre(double d) => defaultkaresz.Előre(d);
+        private void Hátra(double d) => defaultkaresz.Hátra(d);
+        private void Jobbra(double d) => defaultkaresz.Jobbra(d);
+        private void Balra(double d) => defaultkaresz.Balra(d);
+        private void Fordulj(double d) => defaultkaresz.Fordulj(d);
+        private void Lépj(double d) => defaultkaresz.Lépj(d);
+        private void Pihi(int i) => defaultkaresz.Pihi(i);
+        private void Tollat(bool b) => defaultkaresz.Tollat(b);
+        private void Tölt(Color c) => defaultkaresz.Tölt(c);
+
+        void Tollszín(Color szín) => defaultkaresz.Tollszín(szín);
+        void Tollszín(int i) => defaultkaresz.Tollszín(i);
+        void Tollvastagság(float v) => defaultkaresz.Tollvastagság(v);
+        private void Ív(double f, double r) => defaultkaresz.Ív(f, r);
+        public class Frissítés : IDisposable
+        {
+            Avatar a;
+            bool fr;
+            public Frissítés(Avatar a, bool fr) { (this.a, this.fr) = (a, a.Állandó_frissítés); a.Állandó_frissítés = fr; }
+            public Frissítés(bool fr) : this(defaultkaresz, fr) { }
+            public void Dispose() { a.Állandó_frissítés = fr; GC.SuppressFinalize(this); }
+        }
+        public class Rajzol : IDisposable
+        {
+            Avatar a;
+            bool rajzole_regi;
+            public Rajzol(Avatar a, bool rajzoljone) { (this.a, this.rajzole_regi) = (a, a.rajzole); a.Tollat(rajzoljone); }
+            public Rajzol(bool r) : this(defaultkaresz, r) { }
+            public void Dispose() { a.Tollat(rajzole_regi); GC.SuppressFinalize(this); }
+        }
+        public class Szín : IDisposable
+        {
+            Avatar a;
+            Color régi_szín;
+            public Szín(Avatar a, Color új_szín) { (this.a, this.régi_szín) = (a, a.toll.Color); a.Tollszín(új_szín); }
+            public Szín(Color új_szín) : this(defaultkaresz, új_szín) { }
+            public void Dispose() { a.Tollszín(régi_szín); GC.SuppressFinalize(this); }
+        }
+        public class Vastagság : IDisposable
+        {
+            Avatar a;
+            float régi_vastagság;
+            public Vastagság(Avatar a, float új_vastagság) { (this.a, this.régi_vastagság) = (a, a.toll.Width); a.Tollvastagság(új_vastagság); }
+            public Vastagság(float új_vastagság) : this(defaultkaresz, új_vastagság) { }
+            public void Dispose() { a.Tollvastagság(régi_vastagság); GC.SuppressFinalize(this); }
+        }
+        public class Vékonyít : IDisposable
+        {
+            Avatar a;
+            float szorzó;
+            public Vékonyít(Avatar a, float szorzó) { (this.a, this.szorzó) = (a, szorzó); a.toll.Width *= szorzó; }
+            public Vékonyít(Avatar a, double szorzó) : this(a, (float)szorzó) { }
+            public Vékonyít(float szorzó) : this(defaultkaresz, szorzó) { }
+            public Vékonyít(double szorzó) : this(defaultkaresz, szorzó) { }
+            public void Dispose() { a.toll.Width /= szorzó; GC.SuppressFinalize(this); }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace LogoKaresz
+{
+	public partial class Form1 : Form
+	{
+		/* Függvények */
+
+
+		/* Függvények vége */
+		void FELADAT()
+		{
+			// using (new Frissítés(false))
+			// Fa1(12, 100);
+			defaultkaresz.varakozas = 10;
+			
+			using (new Vastagság(5))
+			balFa2(4, 100);
+			
+		}
+		int vált(int szint) => 2 * (szint % 2) - 1;
+		void balFa2(int szint, double méret)
+		{
+			using (new Vékonyít(.7))
+			{
+				if (szint > 0)
+				{
+					using (new Szín(Color.Red))
+					{
+						Előre(méret / 2);
+						erre_jobbFa2(60, szint - 1, méret / 2);
+						Előre(méret / 2);
+						erre_jobbFa2(45, szint - 1, méret / 2);
+						erre_balFa2(-45, szint - 1, méret / 2);
+						Hátra(méret);
+					}
+				}
+			}
+		}
+		void jobbFa2(int szint, double méret)
+		{
+			using (new Vékonyít(.7))
+			{
+				if (szint > 0)
+				{
+					using (new Szín(Color.Blue))
+					{
+						Előre(méret / 2);
+						erre_balFa2(-60, szint - 1, méret / 2);
+						Előre(méret / 2);
+						erre_jobbFa2(45, szint - 1, méret / 2);
+						erre_balFa2(-45, szint - 1, méret / 2);
+						Hátra(méret);
+					}
+				}
+			}
+		}
+
+		void Fa1(int szint, double méret)
+		{
+			if (szint > 0)
+			{
+				Előre(méret / 2);
+				erre_Fa1(60, szint - 2, méret / 2);
+				Előre(méret / 2);
+				erre_Fa1(45, szint - 1, méret / 2);
+				erre_Fa1(-45, szint - 1, méret / 2);
+				Hátra(méret);
+			}
+		}
+		void erre_Fa1(int fok, int szint, double méret)
+		{
+			Balra(fok);
+			Fa1(szint, méret);
+			Jobbra(fok);
+		}
+		void erre_balFa2(int fok, int szint, double méret)
+		{
+			Balra(fok);
+			balFa2(szint, méret);
+			Jobbra(fok);
+		}
+
+		void erre_jobbFa2(int fok, int szint, double méret)
+		{
+			Balra(fok);
+			jobbFa2(szint, méret);
+			Jobbra(fok);
+		}
+
+
+
+
+	}
+}
